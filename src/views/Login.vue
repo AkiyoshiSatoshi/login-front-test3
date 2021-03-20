66f6f992419afd0ac823ba3b1b4312cd6d950d48
Akiyoshi Satoshi <akiyoshiakiyoshi3104@gmail.com>
2021-03-20 18:49:33 +0900

    first-message

---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div>
+      <label for="">
+        <input type="email" class="email" v-model="email">
+        <p>Email</p>
+      </label>
+    </div>
+    <div>
+      <label for="">
+        <input type="password" v-model="password">
+        <p>Password</p>
+      </label>
+    </div>
+    <div>
+      <label for="">
+        <button class="login--btn" @click="login()">Login</button>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async login() {
+      
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.login--btn {
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: rgb(151, 150, 150) 1px solid;
+}
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div>
+      <label for="">
+        <input type="email" class="email" v-model="email">
+        <p>Email</p>
+      </label>
+    </div>
+    <div>
+      <label for="">
+        <input type="password" v-model="password">
+        <p>Password</p>
+      </label>
+    </div>
+    <div>
+      <label for="">
+        <button class="register--btn" @click="register()">Register</button>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async register() {
+      axios.post()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.register-btn {
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: rgb(151, 150, 150) 1px solid;
+}
+
+</style>
